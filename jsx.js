6f90f286f336374769c0dcf8790d34a3f99113b4
Hans Felde <m33pfish@gmail.com>
2016-02-10 19:09:50 -0600

    Update jsx.js

--- jsx.js
+++ jsx.js
@@ -5,7 +5,7 @@ var jsxProcess = (function() {
   var atThis = s => s.replace(/@(?=[^"]*(?:"[^"]*"[^"]*)*$)/g, "this");
   var dColonBind = s => s.replace(/([\w$(){}[\]'"]+)::([\w$(){}[\]'"]+)(?=[^"]*(?:"[^"]*"[^"]*)*$)/g, "$1.bind($2)");
   var dotEquals = s => s.replace(/([\w$(){}[\]'"]+) \.= ([\w$(){}[\]'"]+)(?=[^"]*(?:"[^"]*"[^"]*)*$)/g, "$1 = $1.$2");
-  return s => dotEquals(dColonbind(atThis(atThisProp(hashProto(hashConstr(s))))));
+  return s => dotEquals(dColonBind(atThis(atThisProp(hashProto(hashConstr(s))))));
 })();
 plugins.add({
   header: "jsx",
